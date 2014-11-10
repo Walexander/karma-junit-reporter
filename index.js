cc44b320c9876d7baf0180cc1fa02b0dff8ac130
Will Alexander <will@beanstockmedia.com>
2014-11-10 17:00:33 -0500

    index.js: create packages and classes

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
 
   var xml;
   var suites;
+  var browsers = [];
   var pendingFileWritings = 0;
   var fileWritingFinished = function() {};
   var allMessages = [];
@@ -24,36 +25,21 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
   }];
 
   var initliazeXmlForBrowser = function(browser) {
-    var timestamp = (new Date()).toISOString().substr(0, 19);
-    var suite = suites[browser.id] = xml.ele('testsuite', {
-      name: browser.name, 'package': pkgName, timestamp: timestamp, id: 0, hostname: os.hostname()
-    });
-    suite.ele('properties').ele('property', {name: 'browser.fullName', value: browser.fullName});
+	  browsers.push( browser.name )
   };
 
   this.onRunStart = function(browsers) {
     suites = Object.create(null);
     xml = builder.create('testsuites');
-
-    // TODO(vojta): remove once we don't care about Karma 0.10
-    browsers.forEach(initliazeXmlForBrowser);
   };
 
   this.onBrowserStart = function(browser) {
-    initliazeXmlForBrowser(browser);
+    //initliazeXmlForBrowser(browser);
   };
 
   this.onBrowserComplete = function(browser) {
-    var suite = suites[browser.id];
-
-    if (!suite) {
-      // This browser did not signal `onBrowserStart`. That happens
-      // if the browser timed out during the start phase.
-      return;
-    }
-
-    var result = browser.lastResult;
-
+    /*
+	 * var suite = suites[browser.id];
     suite.att('tests', result.total);
     suite.att('errors', result.disconnected || result.error ? 1 : 0);
     suite.att('failures', result.failed);
@@ -61,6 +47,7 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
 
     suite.ele('system-out').dat(allMessages.join() + '\n');
     suite.ele('system-err');
+	*/
   };
 
   this.onRunComplete = function() {
@@ -86,20 +73,45 @@ var JUnitReporter = function(baseReporterDecorator, config, logger, helper, form
   };
 
   this.specSuccess = this.specSkipped = this.specFailure = function(browser, result) {
-    var spec = suites[browser.id].ele('testcase', {
-      name: result.description, time: ((result.time || 0) / 1000),
-      classname: (pkgName ? pkgName + ' ' : '') + browser.name + '.' + result.suite.join(' ').replace(/\//g, '.')
-    });
+	var testName = result.suite.join(' ').replace(/\//g, '.')
+	var browserName = browser.name.replace(/\..*$/g, '').toLowerCase()
+	var package = testName.split(/[^\w]/)[0]
+	suiteName = testName.replace(/[^a-zA-Z0-9\/\.].+$/, '')
+
+	var suite = suites[suiteName] = suites[suiteName] || xml.ele('testsuite', {
+		name: suiteName,
+		package: package,
+		timestamp: (new Date()).toISOString().substr(0,19),
+		hostname: os.hostname(),
+		id: 0,
+		tests: 0,
+		errors: 0,
+		failures: 0
+	})
+	var total = suite.total || 0
+	var errors = suite.errors || 0
+	var failures = suite.failures | 0
+	var spec = suite.ele('testcase', {
+		name: testName + ': ' + result.description,
+		time: ((result.time||0))/1000,
+		classname: browserName
+	})
+	suite.att('tests', total++)
+	suite.total = total
 
     if (result.skipped) {
       spec.ele('skipped');
     }
 
+
     if (!result.success) {
       result.log.forEach(function(err) {
         spec.ele('failure', {type: ''}, formatError(err));
       });
+	  suite.att('failures', failures++)
+	  suite.failures = failures
     }
+
   };
 
   // wait for writing all the xml files, before exiting
